796972babc5dca8b3d70aa3b456a33b45e0971dd
nick-dolf <nickdolf@gmail.com>
2022-01-02 18:34:26 -0600

    added git pull command for home project

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const exec = require('child_process').exec
 
 const app = express()
 const PORT = 50000
 
 app.use(bodyParser.json())
 
-app.post('/hook/home', (req, res) => {
+app.post('/home', (req, res) => {
 	console.log('received webhook')
+	console.log(req.body)
+	exec('cd ~/home && git pull')
 	res.status(200).end()
 })
 

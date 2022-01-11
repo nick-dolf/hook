9cf101a1bc2e2ad568f3ad6efed045fe98e42622
nick-dolf <nickdolf@gmail.com>
2022-01-11 10:34:31 -0600

    fixed shame typo

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.post('/home', (req, res) => {
 	res.status(200).end()
 })
 
-app.post(/ceic-jekyll', (req, res) => {
+app.post('/ceic-jekyll', (req, res) => {
 	console.log('received webhook')
 	console.log(req.body)
 	exec('cd ~/c4c/ceic-jekyll && git pull')

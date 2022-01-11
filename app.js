f33861f5ba642b92f5270fc68b421a6094f76140
nick-dolf <nickdolf@gmail.com>
2022-01-11 10:29:37 -0600

    added hook for ceic-jekyll and build

--- app.js
+++ app.js
@@ -14,4 +14,12 @@ app.post('/home', (req, res) => {
 	res.status(200).end()
 })
 
+app.post(/ceic-jekyll', (req, res) => {
+	console.log('received webhook')
+	console.log(req.body)
+	exec('cd ~/c4c/ceic-jekyll && git pull')
+	exec('cd ~/c4c/ceic-jekyll && jekyll build -d ~/ceic-jekyll -b https://nickdolf.com/ceic-jekyll')
+	res.status(200).end()
+})
+
 app.listen(PORT, 'localhost', () => console.log(`listening on ${PORT}`))

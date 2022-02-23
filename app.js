b5ea7a20e85775b4c421dfe4ed89ce5622c24d37
nick-dolf <nickdolf@gmail.com>
2022-02-23 12:07:54 -0600

    added webhook for this app

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const PORT = 50000
 
 app.use(bodyParser.json())
 
+// Home Page
 app.post('/home', (req, res) => {
-	console.log('received webhook')
-	console.log(req.body)
 	exec('cd ~/home && git pull')
 	res.status(200).end()
 })
 
+// Webhook handler
+app.post('/hook', (req, res) => {
+	exec('cd ~/hook && git pull')
+	res.status(200).end()
+})
+
+// Staging Site for Code for Chicago CEIC project
 app.post('/ceic-jekyll', (req, res) => {
-	console.log('received webhook')
-	console.log(req.body)
 	exec('cd ~/c4c/ceic-jekyll && git pull')
 	exec('cd ~/c4c/ceic-jekyll && jekyll build -d ~/c4c/jekyll-ceic -b /jekyll-ceic')
 	res.status(200).end()

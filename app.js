a56aa53fae3ed35b50c1d0122b2ea791b0e26e2a
nick-dolf <nickdolf@gmail.com>
2022-04-05 14:27:04 -0700

    changed ceic-jekyll build command to bundle exec

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.post('/tatlo', (req, res) => {
 // Staging Site for Code for Chicago CEIC project
 app.post('/ceic-jekyll', (req, res) => {
 	exec('cd ~/c4c/ceic-jekyll && git pull')
-	exec('cd ~/c4c/ceic-jekyll && jekyll build -d ~/c4c/jekyll-ceic -b /jekyll-ceic')
+	exec('cd ~/c4c/ceic-jekyll && bundle exec jekyll build -d ~/c4c/jekyll-ceic -b /jekyll-ceic')
 	res.status(200).end()
 })
 
